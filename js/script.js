714f72a5cbb2dc8b7a154e1dcefaef7718651e6f
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-09-02 13:21:50 +0600

    error handling

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ const search = () => {
   loadData(inputValue);
 }
 
-document.getElementById('input-field').addEventListener('keypress',(e)=>{
+document.getElementById('input-field').addEventListener('keypress', (e) => {
   const inputValue = document.getElementById('input-field').value;
-  if(e.key === 'Enter'){
+  if (e.key === 'Enter') {
     loadData(inputValue);
   }
 })
@@ -14,15 +14,20 @@ const loadData = city => {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1e4d4e6d4b381fba5d999374a5dd1dc4`)
     .then(res => res.json())
     .then(data => displayData(data))
-    .catch(error => console.log(error))
 }
 
-const displayData = data =>{
+const displayData = data => {
   const city = document.getElementById('city');
+  city.textContent = '';
   const temperature = document.getElementById('temperature');
+  temperature.textContent = '';
   const description = document.getElementById('description');
+  description.textContent = '';
+  if (data.cod == 404) {
+    city.innerText = "City not found !!"
+    return;
+  }
   city.innerText = data.name;
-  temperature.innerText = data.main.temp;
-  description.innerText = data.weather[0].description;
-  console.log(data);
+  temperature.innerText = data.main?.temp;
+  description.innerText = data?.weather?.[0].description;
 }
